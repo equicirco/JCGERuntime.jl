d9764ffeb2ea705e749f0df5a1c20f6111b3f35a
Riccardo Boero <ribo@nilu.no>
2026-01-11 15:47:10 -0500

    fix logos

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,12 @@ makedocs(
     sitename = "JCGERuntime",
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", "false") == "true",
-        assets = ["assets/deepwiki-chat.css", "assets/deepwiki-chat.js", "assets/logo-theme.js", "assets/jcge_runtime_logo_light.png", "assets/jcge_runtime_logo_dark.png"],
+        assets = [
+            "assets/deepwiki-chat.css",
+            "assets/deepwiki-chat.js",
+            "assets/logo-theme.js",
+            "assets/logo.css",
+        ],
     ),
     pages = [
         "Home" => "index.md",
@@ -16,6 +21,7 @@ makedocs(
 
 
 deploydocs(
-    repo = "github.com/equicirco/JCGERuntime.jl",
+    repo = "github.com/equicirco/JCGERuntime.jl.git",
+    devbranch = "main",
     versions = ["stable" => "v^", "v#.#", "dev" => "dev"],
 )
